31a1b3f2b879c57806f4d61ffbe55137987d4c51
Victor Hugo <victorhdoliveira@gmail.com>
2022-10-19 16:25:33 -0300

    Requisito nº 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+describe('teste do componente FavoritePokemons', () => {
+  it('Verifica se é exibida a mensagem corretamente, caso não haja Pokemons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavorite = screen.getByText('No favorite pokemon found');
+    expect(noFavorite).toBeInTheDocument();
+  });
+  it('Verifica se Pokemons são salvos corretamente', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const moreDetails = screen.getByRole('link', { name: /details/i });
+    userEvent.click(moreDetails);
+
+    const fav = screen.getByRole('checkbox');
+    userEvent.click(fav);
+
+    act(() => {
+      history.push('/favorites');
+    });
+
+    const img = screen.getByAltText(/is marked as favorite/);
+    expect(img).toBeInTheDocument();
+  });
+});
